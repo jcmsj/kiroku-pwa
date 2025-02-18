85e3dd04df02aa3b157fac25f35bab9fef0b137c
= <sanjuan.jeancarlo@gmail.com>
2025-02-18 18:46:25 +0800

    Increase maximum file size to cache in PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       // https://vite-pwa-org.netlify.app/guide/faq.html#missing-assets-from-sw-precache-manifest
       workbox: {
-        maximumFileSizeToCacheInBytes: 30_000_000
+        maximumFileSizeToCacheInBytes: 48_000_000
       }
      }),
     // Need to polyfill buffer for use by @jimp/wasm-webp
